b3f8a8b6e0a33a263c1d67754c131410aa7bc46f
yckuo <yckuo@nics.nat.gov.tw>
2023-08-17 12:02:05 +0800

    Modify webshell detection model

--- webshell_detection_model.py
+++ webshell_detection_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 
 from package.ip2gov_adapter import Ip2govAdapter 
 from package.virustotal import VirusTotal
+from package.boturl_validator import BoturlValidator
 from package.network_utility import NetworkUtility
 import helper.http_validator as http
 import helper.network_validator as network
@@ -10,6 +11,7 @@ import numpy as np
 def get_webshell_labels(docs, early_stopping=True):
     vt = VirusTotal()
     ip2gov = Ip2govAdapter()
+    bt = BoturlValidator()
 
     label_keys = ['is_gov', 'network_reachable', 'http_success', 'filehash_malicious', 'boturl_malicious'] 
     labels = []
@@ -26,13 +28,13 @@ def get_webshell_labels(docs, early_stopping=True):
                 label['http_success'] = http.is_successful(doc['request'])
                 if label['http_success']:
                     label['filehash_malicious'] = vt.is_malicious_by_filehash(filehash)
-                    label['boturl_malicious'] = False 
+                    label['boturl_malicious'] = bt.is_malicious_by_boturl(doc['request'], doc['cs8'])
         else: 
             label['is_gov'] = ip2gov.is_gov(service['ip'])
             label['network_reachable'] = network.is_opened(doc['dst'], int(doc['dpt']))
             label['http_success'] = http.is_successful(doc['request'])
             label['filehash_malicious'] = vt.is_malicious_by_filehash(doc['fileHash'])
-            label['boturl_malicious'] = False 
+            label['boturl_malicious'] = bt.is_malicious_by_boturl(doc['request'], doc['cs8'])
 
         labels.append(label)
 
