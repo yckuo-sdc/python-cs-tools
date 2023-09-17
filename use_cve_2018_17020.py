c6c54e5d369277254d94fb6c31fb8154aacc261f
yckuo <yckuo@nics.nat.gov.tw>
2023-09-17 19:54:50 +0800

    Update use_cve_2018_17020.py

--- use_cve_2018_17020.py
+++ use_cve_2018_17020.py
@@ -16,7 +16,8 @@ ACCESSIBLE_CODES = [200, 401]
 def get_response_code(request_url):
     """Function get http status code."""
     try:
-        response = requests.get(request_url, allow_redirects=True, timeout=5)
+        # ignore verifying the SSL certificate
+        response = requests.get(request_url, allow_redirects=True, verify=False, timeout=5)
         return response.status_code
     except Exception as e:
         print(e)
@@ -24,7 +25,7 @@ def get_response_code(request_url):
 
 
 def is_accessible(request_url):
-    """Function confirm that web service is accessible."""
+    """Function confirm that web field is accessible."""
     code = get_response_code(request_url)
     if code not in ACCESSIBLE_CODES:
         return False
@@ -36,47 +37,90 @@ if __name__ == '__main__':
 
     ip2gov = Ip2govAdapter()
     sa = ShodanAdapter()
+
+    # filer 1
+    #search_filters = {
+    #    'country': 'tw',
+    #    'org': "Government Service Network (GSN)",
+    #    'os': 'ASUSWRT',
+    #}
+
+    # filer 2
+    #search_filters = {
+    #    'country': 'tw',
+    #    'org': 'Government Service Network (GSN)',
+    #    'all': 'HTTP/1.0 200 OK Server: httpd/2.0',
+    #    'port': '8080',
+    #}
+
+    # filer 3
     search_filters = {
         'country': 'tw',
-        'org': "Government Service Network (GSN)",
-        'os': 'ASUSWRT',
+        'org': 'Government Service Network (GSN)',
+        'all': 'HTTP/1.0 200 OK Server: httpd/2.0',
     }
 
     match_fields = [
         {'label': 'ip', 'field': 'ip_str'},
         {'label': 'port', 'field': 'port'},
+        {'label': 'product', 'field': 'product'},
+        {'label': 'ssl', 'field': 'ssl'},
     ]
 
-    services = sa.basic_query(search_filters, match_fields)
-    print(services)
+    fields = sa.basic_query(search_filters, match_fields)
+    print(fields)
 
+    IPERF3_PATH = 'set_iperf3_svr.cgi'
     output = []
-    for service in services:
+    for field in fields:
         EXPLOITABLE = False
-        host = service['ip']
-        port = service['port']
-        print(host, port)
+        IPERF3_RESOURCE = False
 
-        if port == 80:
-            url = f"http://{host}"
-        else: 
+        host = field['ip']
+        port = field['port']
+
+        if field['ssl'] is None:
+            field['ssl'] = False
             url = f"http://{host}:{port}"
-
-        gov_data = ip2gov.get_gov_data_by_ip(service['ip'])
-        if gov_data:
-            dep = gov_data.get('DEP')
         else:
-            dep = None
+            field['ssl'] = True
+            url = f"https://{host}:{port}"
+
+        field['url'] = url
+        print(host, port, url)
+
+        gov_data = ip2gov.get_gov_data_by_ip(field['ip'])
+        if gov_data:
+            department = gov_data.get('DEP')
+            department_class = gov_data.get('Class')
+            acc = gov_data.get('ACC') 
+        else:
+            department = None
+            department_class = None 
+            acc = None
+
+        # [check 1]
+        IPERF3_RESOURCE = is_accessible(f"{url}/{IPERF3_PATH}")
+        IPERF3_RESOURCE_CODE = get_response_code(f"{url}/{IPERF3_PATH}")
 
         FIRST_VISIT = is_accessible(url)
         print(f"first_visit: {FIRST_VISIT}")
 
         if not FIRST_VISIT:
-            print("exit")
-            output.append(service | {'department': dep} | {'exploitable': EXPLOITABLE})
-            #output.append(service | {'exploitable': EXPLOITABLE})
+            print("early drop")
+            output.append( 
+                    field | \
+                    {'department': department} | \
+                    {'acc': acc} | \
+                    {'class': department_class} | \
+                    {'cve-2018-17020': EXPLOITABLE} | \
+                    {'found_iperf3_cgi': IPERF3_RESOURCE} | \
+                    {'http_code_of_iperf3_cgi': IPERF3_RESOURCE_CODE}
+            )
             continue
 
+
+        # [check 2]
         print('start socket')
         # Create a socket object
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -104,10 +148,17 @@ if __name__ == '__main__':
         client_socket.close()
         print('close socket')
 
-        #output.append(service | {'exploitable': EXPLOITABLE})
-        output.append(service | {'department': dep} | {'exploitable': EXPLOITABLE})
+        output.append(
+                field | \
+                {'department': department} | \
+                {'acc': acc} | \
+                {'class': department_class} | \
+                {'cve-2018-17020': EXPLOITABLE} | \
+                {'found_iperf3_cgi': IPERF3_RESOURCE} | \
+                {'http_code_of_iperf3_cgi': IPERF3_RESOURCE_CODE}
+        )
 
     output_dict = func.arr_dict_to_flat_dict(output)
     df = pd.DataFrame(output_dict)
     path_to_csv = os.path.join(os.path.dirname(__file__), "data", "use_cve_2018_17020.csv")
-    df.to_csv(path_to_csv, index=False)
+    df.to_csv(path_to_csv, index=False, encoding='utf-8-sig')
