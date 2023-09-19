0972ad29c82ec2567aed70ea9a431723f687a378
yckuo <t910729@gmail.com>
2023-09-19 05:09:04 +0000

    Update on 20230919

--- helper/http_validator.py
+++ helper/http_validator.py
@@ -1,49 +1,71 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 import hashlib
 import re
 from urllib.parse import urlparse
 
 import requests
-
 from bs4 import BeautifulSoup
 
 SUCCESS_CODES = range(200, 300)  # 200-299
 
+
 def is_bot_useragent(useragent):
-    pattern = '\([^)]+\)|\S+\/'
+    pattern = r"\([^)]+\)|\S+\/"
     result = re.findall(pattern, useragent)
     useragent_count = len(result)
-    print('{}: {}'.format(useragent, useragent_count))
+    print(f"{useragent}: {useragent_count}")
     if useragent_count <= 5:
-      return True
+        return True
     return False
 
-def find_text_on_tags(html_doc, text): 
+
+def find_text_on_divs(html_doc, div_class):
     soup = BeautifulSoup(html_doc, 'html.parser')
-    matched_tags = soup.find_all(lambda tag: len(tag.find_all()) == 0 and text.lower() in tag.get_text().lower())
+    matched_tag = soup.find("div", {"class": "prod_madelName"})
+
+    if matched_tag:
+        return matched_tag.get_text()
+
+    return None
+
+
+def find_text_on_tags(html_doc, text):
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    matched_tags = soup.find_all(lambda tag: len(tag.find_all()) == 0 and text.
+                                 lower() in tag.get_text().lower())
 
     for matched_tag in matched_tags:
-        print("Matched: {}".format(matched_tag))
+        print(f"Matched: {matched_tag}")
 
     if len(matched_tags) > 0:
         return True
 
     return False
 
+
 def is_routing_to_error_page(url):
     try:
-        error_msgs = ['404', 'not found', 'invalid', 'error', '失效', '無效', '不存在']
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
-        r = requests.get(url, headers=headers, allow_redirects=False, timeout=10)
+        error_msgs = [
+            '404', 'not found', 'invalid', 'error', '失效', '無效', '不存在'
+        ]
+        headers = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+        }
+        r = requests.get(url,
+                     headers=headers,
+                     allow_redirects=False,
+                     timeout=10)
         for error_msg in error_msgs:
             if find_text_on_tags(r.text, error_msg):
                 print('It is routing to error page')
                 return True
-        return False 
+        return False
     except Exception as e:
         print(e)
         return False
-    
+
+
 def is_routing_to_homepage(url):
     obj = urlparse(url)
     if obj.path == "" and obj.params == "" and obj.query == "" and obj.fragment == "":
@@ -51,45 +73,95 @@ def is_routing_to_homepage(url):
 
     homepage = obj.scheme + '://' + obj.netloc
     try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
-        homepage_footprint = hashlib.sha1(requests.get(homepage, headers=headers, allow_redirects=True, timeout=10).text.encode('utf-8')).hexdigest() 
-        url_footprint = hashlib.sha1(requests.get(url, headers=headers, allow_redirects=False, timeout=10).text.encode('utf-8')).hexdigest() 
+        headers = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+        }
+        homepage_footprint = hashlib.sha1(
+            requests.get(homepage,
+                         headers=headers,
+                         allow_redirects=True,
+                         timeout=10).text.encode('utf-8')).hexdigest()
+        url_footprint = hashlib.sha1(
+            requests.get(url,
+                         headers=headers,
+                         allow_redirects=False,
+                         timeout=10).text.encode('utf-8')).hexdigest()
 
         if homepage_footprint == url_footprint:
             print('It is routing to homepage')
             return True
 
-        return False 
+        return False
     except Exception as e:
         print(e)
         return False
+
 
 def is_custom_error_handling(url):
     if is_routing_to_homepage(url):
         return True
     if is_routing_to_error_page(url):
         return True
-    
+
     return False
 
-def get_response_code(url):  
+
+def get_response_body(url):
     try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
-        r = requests.get(url, headers=headers, allow_redirects=False, timeout=10)
-        return r.status_code
+        headers = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+        }
+        r = requests.get(url,
+                         headers=headers,
+                         allow_redirects=True,
+                         verify=False,
+                         timeout=10)
+        return r.text
     except Exception as e:
         print(e)
         return False
 
 
-def is_accessable(url):
-    print("request: {}".format(url))
+def get_response_code(url):
+    try:
+        headers = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+        }
+        # ignore verifying the SSL certificate
+        response = requests.get(url,
+                                headers=headers,
+                                allow_redirects=True,
+                                verify=False,
+                                timeout=5)
+        return response.status_code
+    except Exception as e:
+        print(e)
+        return False
+
+
+def get_js_redirect_url(url):
+    html_doc = get_response_body(url)
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    script = soup.find('script')
+    pattern = "href=\'([^']+)\'"
+    result = re.findall(pattern, script.get_text())
+    if result:
+        return result[0]
+
+    return False
+
+
+def is_successful(url):
+    print(f"request: {url}")
     code = get_response_code(url)
-    print("code: {}".format(code))
+    print(f"code: {code}")
     if not code:
         return False
-   
-    if code not in SUCCESS_CODES: 
+
+    if code not in SUCCESS_CODES:
         return False
 
     if code == 200:
@@ -99,7 +171,7 @@ def is_accessable(url):
     return True
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
 
     html_doc = """
     <div>
--- use_cve_2018_17020.py
+++ use_cve_2018_17020.py
@@ -1,32 +1,20 @@
 """Module Proof of Concept for CVE-2018-17020."""
 import os
 import socket
-import sys
 
 import pandas as pd
-import requests
 
 import helper.function as func
-from package.shodan_adapter import ShodanAdapter
+import helper.http_validator as http
 from package.ip2gov_adapter import Ip2govAdapter
+from package.shodan_adapter import ShodanAdapter
 
 ACCESSIBLE_CODES = [200, 401]
 
 
-def get_response_code(request_url):
-    """Function get http status code."""
-    try:
-        # ignore verifying the SSL certificate
-        response = requests.get(request_url, allow_redirects=True, verify=False, timeout=5)
-        return response.status_code
-    except Exception as e:
-        print(e)
-        return False
-
-
 def is_accessible(request_url):
     """Function confirm that web field is accessible."""
-    code = get_response_code(request_url)
+    code = http.get_response_code(request_url)
     if code not in ACCESSIBLE_CODES:
         return False
 
@@ -61,10 +49,22 @@ if __name__ == '__main__':
     }
 
     match_fields = [
-        {'label': 'ip', 'field': 'ip_str'},
-        {'label': 'port', 'field': 'port'},
-        {'label': 'product', 'field': 'product'},
-        {'label': 'ssl', 'field': 'ssl'},
+        {
+            'label': 'ip',
+            'field': 'ip_str'
+        },
+        {
+            'label': 'port',
+            'field': 'port'
+        },
+        {
+            'label': 'product',
+            'field': 'product'
+        },
+        {
+            'label': 'ssl',
+            'field': 'ssl'
+        },
     ]
 
     fields = sa.basic_query(search_filters, match_fields)
@@ -75,6 +75,10 @@ if __name__ == '__main__':
     for field in fields:
         EXPLOITABLE = False
         IPERF3_RESOURCE = False
+        #DEPARTMENT = None
+        #DEPARTMENT_CLASS = None
+        #ACC = None
+        MODEL_NAME = None
 
         host = field['ip']
         port = field['port']
@@ -89,38 +93,41 @@ if __name__ == '__main__':
         field['url'] = url
         print(host, port, url)
 
-        gov_data = ip2gov.get_gov_data_by_ip(field['ip'])
-        if gov_data:
-            department = gov_data.get('DEP')
-            department_class = gov_data.get('Class')
-            acc = gov_data.get('ACC') 
-        else:
-            department = None
-            department_class = None 
-            acc = None
+        #gov_data = ip2gov.get_gov_data_by_ip(field['ip'])
+        #if gov_data:
+        #    DEPARTMENT = gov_data.get('DEP')
+        #    DEPARTMENT_CLASS = gov_data.get('Class')
+        #    ACC = gov_data.get('ACC')
 
-        # [check 1]
+        # check 1
         IPERF3_RESOURCE = is_accessible(f"{url}/{IPERF3_PATH}")
-        IPERF3_RESOURCE_CODE = get_response_code(f"{url}/{IPERF3_PATH}")
+        IPERF3_RESOURCE_CODE = http.get_response_code(f"{url}/{IPERF3_PATH}")
 
         FIRST_VISIT = is_accessible(url)
         print(f"first_visit: {FIRST_VISIT}")
 
         if not FIRST_VISIT:
             print("early drop")
-            output.append( 
+            output.append(
                     field | \
-                    {'department': department} | \
-                    {'acc': acc} | \
-                    {'class': department_class} | \
+            #        {'department': DEPARTMENT} | \
+            #        {'acc': ACC} | \
+            #        {'class': DEPARTMENT_CLASS} | \
                     {'cve-2018-17020': EXPLOITABLE} | \
                     {'found_iperf3_cgi': IPERF3_RESOURCE} | \
-                    {'http_code_of_iperf3_cgi': IPERF3_RESOURCE_CODE}
+                    {'http_code_of_iperf3_cgi': IPERF3_RESOURCE_CODE} | \
+                    {'model_name': MODEL_NAME}
             )
             continue
 
+        path = http.get_js_redirect_url(url)
+        login_url = f"{url}{path}"
+        print(login_url)
+        DIV_CLASS = "prod_madelName"
+        html_doc = http.get_response_body(login_url)
+        MODEL_NAME = http.find_text_on_divs(html_doc, DIV_CLASS)
 
-        # [check 2]
+        # check 2
         print('start socket')
         # Create a socket object
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -150,15 +157,17 @@ if __name__ == '__main__':
 
         output.append(
                 field | \
-                {'department': department} | \
-                {'acc': acc} | \
-                {'class': department_class} | \
+        #        {'department': DEPARTMENT} | \
+        #        {'acc': ACC} | \
+        #        {'class': DEPARTMENT_CLASS} | \
                 {'cve-2018-17020': EXPLOITABLE} | \
                 {'found_iperf3_cgi': IPERF3_RESOURCE} | \
-                {'http_code_of_iperf3_cgi': IPERF3_RESOURCE_CODE}
+                {'http_code_of_iperf3_cgi': IPERF3_RESOURCE_CODE} | \
+                {'model_name': MODEL_NAME}
         )
 
     output_dict = func.arr_dict_to_flat_dict(output)
     df = pd.DataFrame(output_dict)
-    path_to_csv = os.path.join(os.path.dirname(__file__), "data", "use_cve_2018_17020.csv")
+    path_to_csv = os.path.join(os.path.dirname(__file__), "data",
+                               "use_cve_2018_17020.csv")
     df.to_csv(path_to_csv, index=False, encoding='utf-8-sig')
