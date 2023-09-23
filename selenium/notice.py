da55c751b091b9fa7822f487eb2705fae449518c
yckuo <yckuo@nics.nat.gov.tw>
2023-09-23 14:50:55 +0800

    Update on 20230923

--- selenium/notice.py
+++ selenium/notice.py
@@ -5,33 +5,55 @@ from string import Template
 
 import pandas as pd
 from dotenv import load_dotenv
+from openpyxl import load_workbook
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
+
+def find_files_with_name(directory, target_name):
+    for file in os.listdir(directory):
+        if file.find(target_name) == 0:
+            return os.path.join(directory, file)
+
+    return False
+
+
+def concatenate_values(series):
+    return ', '.join(series)
+
 load_dotenv()
-HOST = os.getenv("NOTICE_HOST")
-USERNAME = os.getenv("NOTICE_USERNAME")
-PASSWORD = os.getenv("NOTICE_PASSWORD")
+HOST = os.getenv('NOTICE_HOST')
+USERNAME = os.getenv('NOTICE_USERNAME')
+PASSWORD = os.getenv('NOTICE_PASSWORD')
 
 # Specify the sheet name or index
-PATH_TO_EXCEL = os.path.join(os.path.dirname(__file__), 'notice_excels', 'asusrt.xlsx')
+PATH_TO_EXCEL = os.path.join(os.path.dirname(__file__), 'notice_excels',
+                             'asusrt.xlsx')
 SHEET1_NAME = '警訊內容'
 SHEET2_NAME = '機關資訊'
+SHEET3_NAME = '執行結果'
 
 # If using openpyxl for .xlsx files
 df1 = pd.read_excel(PATH_TO_EXCEL, engine='openpyxl', sheet_name=SHEET1_NAME)
 df2 = pd.read_excel(PATH_TO_EXCEL, engine='openpyxl', sheet_name=SHEET2_NAME)
 
+## Grouping by multiple columns
+df2['port'] = df2['port'].astype(str)
+grouped_data = df2.groupby('name')
+df2 = grouped_data.agg({
+    'ip': concatenate_values,
+    'port': concatenate_values
+}).reset_index()
+
 form_inputs = df1.to_dict('records')
 form_inputs = next(iter(form_inputs), None)
 deparments = df2.to_dict('records')
-print(form_inputs)
 print(deparments)
 
-## Run Browser in background
+### Run Browser in background
 options = webdriver.ChromeOptions()
 #options.add_argument('--headless')
 options.add_argument('--no-sandbox')
@@ -46,9 +68,9 @@ driver = webdriver.Chrome(options=options, service=service)
 # Setting an implicit wait of 2 seconds
 driver.implicitly_wait(2)
 
-# Directing the driver to the defined url
 
-### Page 1. Login ###
+### Part 1. Login ###
+# Directing the driver to the defined url
 driver.get(HOST)
 print(driver.title)
 
@@ -63,31 +85,48 @@ driver.find_element(By.NAME, "systemUser.password").send_keys(PASSWORD)
 # Click on the element
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
+execution_results = []
 for deparment in deparments:
-    ### Page 2. Classification ###
+    ### Part 2. Classification ###
+    print(deparment['name'])
+
+    attach_directory = os.path.join(os.path.dirname(__file__), 'attachments', 'asusrt')
+    file_name = f"{deparment['name']}.csv"
+    file_path = find_files_with_name(attach_directory, file_name)
+    if not file_path:
+        print(f"Can't find attachment: {file_name}")
+        continue
+
+    # Directing the driver to the defined url
     driver.get(f"{HOST}/notice/Notice.do?method:publishNoticeStep1")
 
+    # noticeCategoryid = {0: '警訊', 2: '國際通報'}
     select_element = driver.find_element(By.NAME, "noticeCategoryid")
     select = Select(select_element)
     select.select_by_value('0')
 
+    # typeId = {1: 'EWA', 3: 'DEF', 4: 'INT', 5: 'ANA'}
     select_element = driver.find_element(By.NAME, "typeId")
     select = Select(select_element)
     select.select_by_value('1')
 
+    # eventId = {165: '其他'}
     select_element = driver.find_element(By.NAME, "eventId")
     select = Select(select_element)
     select.select_by_value('165')
 
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
-    ### Page 3. Filling ###
-    subject = Template(form_inputs['subject']).substitute(department_name=deparment['name'])
-    content = Template(form_inputs['content']).substitute(department_ip=deparment['ip'])
+    ### Part 3. Filling ###
+    subject = Template(
+        form_inputs['subject']).substitute(department_name=deparment['name'])
+    content = Template(
+        form_inputs['content']).substitute(department_ip=deparment['ip'])
     suggestion = form_inputs['suggestion']
     reference = form_inputs['reference']
-    ips = deparment['ip'].split(",")
+    ips = deparment['ip'].split(", ")
 
+    # notice.source = ['外部資安組織', '本院自行發現', 'GSN威脅情蒐機制']
     select_element = driver.find_element(By.NAME, "notice.source")
     select = Select(select_element)
     select.select_by_value('本院自行發現')
@@ -96,10 +135,12 @@ for deparment in deparments:
     driver.find_element(By.NAME, "detectTime").send_keys(today)
     driver.find_element(By.NAME, "notice.subject").send_keys(subject)
 
+    # notice.severity = ['高', '中', '低']
     select_element = driver.find_element(By.NAME, "notice.severity")
     select = Select(select_element)
     select.select_by_value('低')
 
+    # notice.restriction = ['公開資訊', '群組資訊', '機密資訊']
     select_element = driver.find_element(By.NAME, "notice.restriction")
     select = Select(select_element)
     select.select_by_value('機密資訊')
@@ -114,11 +155,38 @@ for deparment in deparments:
     driver.find_element(By.NAME, "notice.reference").send_keys(reference)
 
     # Specify the path to the file you want to upload
-    file_path = os.path.join(os.path.dirname(__file__), 'attachments/asusrt', deparment['upload_file'])
     driver.find_element(By.NAME, "myFile").send_keys(file_path)
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
-    ### Page 4. Preview ###
-    #driver.find_element(By.XPATH, "//input[@type='submit']").click()
+    input("Press Enter to continue...")
+
+    #### Part 4. Preview ###
+    driver.find_element(By.XPATH, "//input[@type='submit']").click()
+
+    #### Part 5. Search Address ###
+    driver.execute_script("goToSelectToPage()")
+    driver.find_element(By.ID, "NoticePrePublish_search").send_keys(deparment['name'])
+    driver.find_element(By.ID, "NoticePrePublish_normal_query").click()
+
+    #### Part 6. Add Address ###
+    checkboxes = driver.find_elements(By.XPATH, "//input[@name='selectGroup']")
+
+    # Iterate through the checkboxes and click each one
+    for checkbox in checkboxes:
+        checkbox.click()
+
+    driver.find_element(By.ID, "NoticePrePublish_normal_ok").click()
+
+    td = driver.find_element(By.XPATH, "//table/tbody/tr[1]/td[1]")
+    notice_id = td.text
+
+    execution_results.append(deparment | {'notice_id': notice_id})
+
+    ### Final. Publish ###
 
     input("Press Enter to continue...")
+
+# Write the modified DataFrame back to the worksheet
+execution_results_df = pd.DataFrame(execution_results)
+with pd.ExcelWriter(PATH_TO_EXCEL, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+    execution_results_df.to_excel(writer, sheet_name=SHEET3_NAME, index=False)
